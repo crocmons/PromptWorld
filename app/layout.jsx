6dc7c0c5c614bf81130f872ef510bdd4a984e95a
Your Name <you@example.com>
2023-07-11 17:40:01 +0600

    updated theme

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,35 +2,32 @@
 import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
-import { ThemeProvider } from "next-themes";
-import { Fragment } from "react";
-import Head from "next/head";
+// import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@components/theme/theme-provider";
 
 export const metadata = {
   title: "PromptMaker",
   description: "Discover & Generate AI Prompts",
 };
 
-const RootLayout = ({ children }) => {
+const RootLayout = ({children}) => {
   return (
-    <ThemeProvider attribute="class">
-      <Fragment>
-        <Head>
-          <title>{metadata.title}</title>
-          <meta name="description" content={metadata.description} />
-        </Head>
+    <html lang="en">
+       <body>
         <Provider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="main">
-            <div className="gradient" />
+              <div className="gradient" />
           </div>
           <main className="app">
             <Nav />
             {children}
           </main>
-        </Provider>
-      </Fragment>
-    </ThemeProvider>
-  );
-};
+       </ThemeProvider>
+          </Provider>
+       </body>
+    </html>
+  )
+}
 
-export default RootLayout;
+export default RootLayout
