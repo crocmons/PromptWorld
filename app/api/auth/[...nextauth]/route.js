c2f4e1095938104d709b39638d8d060443f36411
Your Name <you@example.com>
2023-07-12 02:42:12 +0600

    chnged route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,53 +1,54 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from 'next-auth/providers/google'
-// import { signIn } from "next-auth/react";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import { connectedToDB } from "@utils/database";
 import User from "@models/user";
 
-const handler = NextAuth({
-    providers:[
-        GoogleProvider({
-            clientId:process.env.GOOGLE_CLIENT_ID,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET
-        })
-    ],
-    callbacks:{
-        async session({session}) {
-           const sessionUser = await User.findOne({
-             email:session.user.email
-           })
-    
-           session.user.id = sessionUser._id.toString();
-    
-           return session;
-        },
-    
-        async signIn({profile}) {
-           try {
-            await connectedToDB();
-            // check if a user already exist
-            const userExists = await User.findOne({
-                email: profile.email
-            })
-              console.log(profile)
-            // if not, create a new user and save it to the DB
-            if(!userExists){
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                });
-    
-            }
-            return true;
-    
-           } catch (error) {
-            console.log(error)
-            return false;
-           }
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      try {
+        const sessionUser = await User.findOne({
+          email: session.user.email,
+        });
+
+        if (sessionUser) {
+          session.user.id = sessionUser._id.toString();
         }
-    }
 
-})
+        return session;
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
 
-export {handler as GET, handler as POST};
+    async signIn({ profile }) {
+      try {
+        await connectedToDB();
+
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
+  },
+});
